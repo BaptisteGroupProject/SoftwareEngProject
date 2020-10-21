2501bb8e71229c8ced40570c75dd2d3462209416
Baptiste <baptiste.sagna@student.manchester.ac.uk>
2020-10-21 18:04:59 +0100

    Added Google map on startup and began to implement current location marker

--- app/src/main/java/com/ASETP/project/MainActivity.java
+++ app/src/main/java/com/ASETP/project/MainActivity.java
@@ -2,25 +2,119 @@ package com.ASETP.project;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+
 
 import com.ASETP.project.base.BaseActivity;
 import com.ASETP.project.databinding.ActivityMainBinding;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 /**
  * @author MirageLe
  */
-public class MainActivity extends BaseActivity<ActivityMainBinding> {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private boolean isPermissionOk = false;
 
+    Location currentLocation;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    private static final int REQUEST_CODE = 101;
+
+
+    /*
     @Override
     protected void init(Bundle bundle) {
 
+    }*/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Retrieve the content view that renders the map.
+        setContentView(R.layout.activity_main);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        fetchLastLocation();
+    }
+
+    private void fetchLastLocation() {
+        String[] permission = new String[]{Manifest.permission.ACCESS_FINE_LOCATION};
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, permission, REQUEST_CODE );
+            return;
+        }
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null){
+                    currentLocation = location;
+                    Toast.makeText(getApplicationContext(), currentLocation.getLatitude()
+                            +""+currentLocation.getLongitude(), Toast.LENGTH_SHORT).show();
+                    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                            .findFragmentById(R.id.map);
+                     mapFragment.getMapAsync(MainActivity.this);
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng current = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+        MarkerOptions markerOptions = new MarkerOptions().position(current)
+                .title("Current Location");
+        googleMap.animateCamera(CameraUpdateFactory.newLatLng(current));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(current, 5));
+        googleMap.addMarker(markerOptions);
+    }
+
+    /**
+     * when permission return
+     *
+     * @param requestCode  the code you request
+     * @param permissions  the permissions you request
+     * @param grantResults return the request result
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode){
+            case REQUEST_CODE:
+                if( grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    fetchLastLocation();
+                }
+                break;
+        }
+
+        /*
+        if (requestCode == 1) {
+            for (int result : grantResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    isPermissionOk = false;
+                    Toast.makeText(MainActivity.this, "please turn on location service", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                isPermissionOk = true;
+            }*/
     }
 
     /**
@@ -39,25 +133,6 @@ public class MainActivity extends BaseActivity<ActivityMainBinding> {
         }
     }
 
-    /**
-     * when permission return
-     *
-     * @param requestCode  the code you request
-     * @param permissions  the permissions you request
-     * @param grantResults return the request result
-     */
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == 1) {
-            for (int result : grantResults) {
-                if (result != PackageManager.PERMISSION_GRANTED) {
-                    isPermissionOk = false;
-                    Toast.makeText(MainActivity.this, "please turn on location service", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                isPermissionOk = true;
-            }
-        }
-    }
+
+
 }
